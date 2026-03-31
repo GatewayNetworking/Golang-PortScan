0ea03ae93356baf38cd7a06690222e813bfb74ef
transcrime <xta12wq7@portforwarding.anonaddy.com>
2026-03-31 19:15:15 -0300

    Add files via upload

--- src/main.go
+++ src/main.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func scanPort(host string, port int, timeout time.Duration) bool {
+func TCPPort(host string, port int, timeout time.Duration) bool {
 	address := fmt.Sprintf("%s:%d", host, port)
 	conn, err := net.DialTimeout("tcp4", address, timeout)
 	if err != nil {
@@ -16,16 +16,39 @@ func scanPort(host string, port int, timeout time.Duration) bool {
 	return true
 }
 
-func main() {
-	var input int
-	input, err := fmt.Scan("%.2f", &input)
+func UDPPort(host string, port int, timeout time.Duration) bool {
+	address := fmt.Sprintf("%s:%d", host, port)
+	conn, err := net.DialTimeout("upd", address, timeout)
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
+}
 
-	host := "120"
+func main() {
+	var host string
+	fmt.Print("Enter IP address to scan: ")
+
+	_, err := fmt.Scanln(&host)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
 	timeout := 1 * time.Second
+	fmt.Printf("Scanning %s...\n", host)
 
 	for port := 1; port <= 1024; port++ {
-		if scanPort(host, port, timeout) {
-			fmt.Printf("Port: %d is OPEN\n", port)
+		if TCPPort(host, port, timeout) {
+			fmt.Printf("TCP Port: %d is OPEN\n", port)
+		}
+	}
+
+	for port := 1; port <= 1024; port++ {
+		if UDPPort(host, port, timeout) {
+			fmt.Printf("UDP Port: %d is OPEN\n", port)
+
 		}
 	}
 }
